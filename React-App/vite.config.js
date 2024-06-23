7cad8843b70f23e82dd05bbecf56ba22f8eb8424
lukerpace <lukerpace@gmail.com>
2024-06-23 13:07:28 -0500

    updates

--- React-App/vite.config.js
+++ React-App/vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5173,
   },
   build: {
-    outDir: 'dist',
+    entry: Path.resolve(__dirname, 'src/main.jsx'),
     },   
 })
